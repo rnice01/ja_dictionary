a3edb5cf5aa8dd1e511d3fb6d25f34e67b2771b9
Rob Nice <rob.w.nice@gmail.com>
2020-03-29 22:29:46 -0500

    remove unused code

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,16 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-
-
-defmodule VocabCreator do
-  alias Ecto.Multi
-  def create_vocab(vocab) do
-    Multi.new()
-    |> Multi.insert(JStudyBlog.Dictionary.Vocab, vocab)
-    |> Repo.transaction()
-  end
-end
-
-#VocabCreator.create_vocab(Enum.at(vocabs, 0))
-Enum.each(vocabs, fn v -> VocabCreator.print_it(v) end)
