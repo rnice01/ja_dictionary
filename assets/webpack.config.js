8a30252f99bf8c4552c472dcac685652d535c3d1
Rob Nice <rob.w.nice@gmail.com>
2020-03-31 23:34:46 -0500

    remove webpack plugins breaking heroku build

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const glob = require('glob');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
@@ -19,15 +18,10 @@ module.exports = (env, options) => ({
         use: {
           loader: 'babel-loader'
         }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
